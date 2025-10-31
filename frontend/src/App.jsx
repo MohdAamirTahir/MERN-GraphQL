5073daed91a17bfeb8b9fb10222f54d6d6aa62f5
Mohd Aamir <zunzd.khan@gmail.com>
2025-10-31 17:10:38 +0530

    minor change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ export default function App() {
         <h1 className="text-4xl font-bold text-indigo-600 tracking-tight">
            User Management Dashboard
         </h1>
-        <p className="text-gray-600 mt-2">Manage users</p>
+
       </header>
 
       <main className="w-full max-w-4xl bg-white shadow-xl rounded-2xl p-6 space-y-8">
