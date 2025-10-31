89642d3a3ec3a36f33a6b98c55671f216bce5d9f
Mohd Aamir <zunzd.khan@gmail.com>
2025-10-31 15:59:43 +0530

    Add taiwind css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"; // correct
 import App from "./App.jsx";
+import "./index.css";
+
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql", // your backend URL
